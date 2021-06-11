d0736328c44987e4f311ba95524954559258b1a0
Francisco Salinas <fsalinas@be-spread.com>
2021-06-11 08:32:09 -0500

    tarea 05

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace dev_homwork05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            bool cesarTinehambre = true;
+
+            while(cesarTinehambre ){
+                Console.WriteLine($"que quieres comer: ");
+                var comida = Console.ReadLine();
+
+                if(comida == "ya no quiero"){
+                    cesarTinehambre = false;
+                }
+                else{
+                    Console.WriteLine("preparando: " + comida);
+                }
+            }
+            
+            
+
+
+        }
+    }
+}
